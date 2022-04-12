a7a54efa9b43f2e0b895e3122be420442dd01f1c
prem jangir <premjangir@prems-MacBook-Pro.local>
2022-04-11 23:12:17 -0700

    fix

--- setup_selection.py
+++ setup_selection.py
@@ -63,7 +63,7 @@ for row in trade_list:
     if historicData['data'] is None:
         continue
 
-    time.sleep(.8)
+    time.sleep(.6)
     intra_df = pd.DataFrame(historicData['data'])
     intra_df = cleanData(intra_df)
     mother_candle = intra_df.iloc[0]
