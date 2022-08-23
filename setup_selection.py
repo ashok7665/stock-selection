da4efe3a0700bec9a4a672f84fb200006faac8f5
premjangir <prem.jangir34@gmail.com>
2022-08-23 11:01:36 +0530

    fixes

--- setup_selection.py
+++ setup_selection.py
@@ -1,4 +1,5 @@
 
+from tkinter import E
 from smartapi import SmartConnect  # or from
 import pandas as pd
 import pymongo
@@ -20,6 +21,9 @@ data = obj.generateSession("P246447", "Jangir76@")
 refreshToken = data['data']['refreshToken']
 
 trade_list = trades.find({"date": "{}".format(today)})
+
+for trade in trade_list:
+    print(trade)
 
 
 def fetchQuantity(price):
@@ -51,59 +55,63 @@ now_datetime = datetime.datetime.now();
 print('\n\n-----------------------------------')
 print("datetime: {}".format(today))
 
-for row in trade_list:
-    time.sleep(0.5)
-    historicParam = {
-        "exchange": "NSE",
-        "symboltoken": row["symbol_token"],
-        "interval": "FIFTEEN_MINUTE",
-        "fromdate": today_date+" 09:15",
-        "todate": today_date+" 09:30",
-    }
-    historicData = obj.getCandleData(historicParam)
-    if historicData['data'] is None:
-        continue
-    intra_df = pd.DataFrame(historicData['data'])
-    intra_df = cleanData(intra_df)
-    mother_candle = intra_df.iloc[0]
-    child_candle = intra_df.iloc[1]
 
-    #print( mother_candle["Low"],child_candle['Low'],  abs(mother_candle['High'] - mother_candle['Low']),CANDLE_PERCENT * mother_candle['Low'],row['trading_symbol'])
-    if mother_candle["Low"] < child_candle['Low'] and mother_candle['High'] > child_candle['High'] and (
-            abs(mother_candle['High'] - mother_candle['Low']) <= CANDLE_PERCENT * mother_candle['Low']):
-        target_ratio = 1.5
-        if mother_candle['High'] > 2000:
-            target_ratio = 2.2
-        buy_price = child_candle['High'] + 1
-        sl = child_candle['Low']
-        quantity = fetchQuantity(buy_price)
-        target = buy_price + (buy_price - sl) * target_ratio
-
-        
-        buy_order = {
-            'buy_price': buy_price,
-            'target': target,
-            "sl": sl,
-            'quantity': quantity,
-            'status': 'pending'
+try:
+    for row in trade_list:
+        time.sleep(0.5)
+        historicParam = {
+            "exchange": "NSE",
+            "symboltoken": row["symbol_token"],
+            "interval": "FIFTEEN_MINUTE",
+            "fromdate": today_date+" 09:15",
+            "todate": today_date+" 09:30",
         }
+        historicData = obj.getCandleData(historicParam)
+        if historicData['data'] is None:
+            continue
+        intra_df = pd.DataFrame(historicData['data'])
+        intra_df = cleanData(intra_df)
+        mother_candle = intra_df.iloc[0]
+        child_candle = intra_df.iloc[1]
 
-        
-        sell_price= child_candle['Low'] -1 
-        sell_sl = child_candle['High']
-        sell_target = child_candle['Low'] - (sell_sl - child_candle['Low']) * target_ratio
-        sell_order = {
-            'sell_price': sell_price,
-            'sl': sell_sl,
-            'target': sell_target,
-            'quantity': quantity,
-            'status': 'pending'
-        }
+        #print( mother_candle["Low"],child_candle['Low'],  abs(mother_candle['High'] - mother_candle['Low']),CANDLE_PERCENT * mother_candle['Low'],row['trading_symbol'])
+        if mother_candle["Low"] < child_candle['Low'] and mother_candle['High'] > child_candle['High'] and (
+                abs(mother_candle['High'] - mother_candle['Low']) <= CANDLE_PERCENT * mother_candle['Low']):
+            target_ratio = 1.5
+            if mother_candle['High'] > 2000:
+                target_ratio = 2.2
+            buy_price = child_candle['High'] + 1
+            sl = child_candle['Low']
+            quantity = fetchQuantity(buy_price)
+            target = buy_price + (buy_price - sl) * target_ratio
 
-        print(row['trading_symbol'])
-        cursor = trades.update_one({"symbol_token": str(row['symbol_token']), "date": today_date}, {
-            "$set": {
-               "status": "order_selected",
-                "buy_order": buy_order,
-                "sell_order": sell_order
-            }},upsert=True)
+            
+            buy_order = {
+                'buy_price': buy_price,
+                'target': target,
+                "sl": sl,
+                'quantity': quantity,
+                'status': 'pending'
+            }
+
+            
+            sell_price= child_candle['Low'] -1 
+            sell_sl = child_candle['High']
+            sell_target = child_candle['Low'] - (sell_sl - child_candle['Low']) * target_ratio
+            sell_order = {
+                'sell_price': sell_price,
+                'sl': sell_sl,
+                'target': sell_target,
+                'quantity': quantity,
+                'status': 'pending'
+            }
+
+            print(row['trading_symbol'])
+            cursor = trades.update_one({"symbol_token": str(row['symbol_token']), "date": today_date}, {
+                "$set": {
+                "status": "order_selected",
+                    "buy_order": buy_order,
+                    "sell_order": sell_order
+                }},upsert=True)
+except Exception as e:
+    print("ERROR ->",e)
